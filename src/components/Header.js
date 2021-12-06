08ed1f4fa72c1029ce049ebec634b4943682e456
Sohom <sohom.datta@learner.manipal.edu>
2021-12-06 19:42:35 +0530

    Added rules and FAQ

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,6 +8,8 @@ function Header() {
       <li><a className="hover-underline-animation" href="https://ctf.cryptonite.team/scoreboard" target="_blank" rel="noreferrer">Scoreboard</a></li>
       <li><a className="hover-underline-animation" href="https://ctf.cryptonite.team/notifications" target="_blank" rel="noreferrer">Notifications</a></li>
       <li><a className="hover-underline-animation" href="https://ctf.cryptonite.team/challenges" target="_blank" rel="noreferrer">Challenges</a></li>
+      <li><a className="hover-underline-animation" href="https://ctf.cryptonite.team/rules" target="_blank" rel="noreferrer">Rules</a></li>
+      <li><a className="hover-underline-animation" href="https://ctf.cryptonite.team/faq" target="_blank" rel="noreferrer">FAQ</a></li>
       <li><a className="hover-underline-animation" href="https://ctf.cryptonite.team/register" target="_blank" rel="noreferrer">Register</a></li>
       <li><a className="hover-underline-animation" href="https://ctf.cryptonite.team/login" target="_blank" rel="noreferrer">Login</a></li>
     </ul>
